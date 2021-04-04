8e1ea8e8b48af15d7882864a481aff6162df348f
Alexander <hello@alexander.works>
2021-04-04 23:43:18 +0300

    Stylize the playground startpage

--- 02-terrain/src/script.js
+++ 02-terrain/src/script.js
@@ -1,10 +1,21 @@
 import './style.css';
 import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+// import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import * as dat from 'dat.gui';
 
 // Debug
 const gui = new dat.GUI();
+gui.close();
+
+const parameters = {
+  pointLightColor: '#2085e6',
+};
+
+// Loaders
+const textureLoader = new THREE.TextureLoader();
+const textureHeight = textureLoader.load('textures/height2.jpeg');
+const textureMap = textureLoader.load('textures/map1.jpeg');
+const textureAlpha = textureLoader.load('textures/alpha1.jpg');
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl');
@@ -13,24 +24,76 @@ const canvas = document.querySelector('canvas.webgl');
 const scene = new THREE.Scene();
 
 // Objects
-const geometry = new THREE.TorusGeometry(0.7, 0.2, 16, 100);
+const geometry = new THREE.PlaneBufferGeometry(3, 3, 64, 64);
 
 // Materials
-
-const material = new THREE.MeshBasicMaterial();
-material.color = new THREE.Color(0xff0000);
+const material = new THREE.MeshStandardMaterial({
+  // color: 'blue',
+  map: textureMap,
+  displacementMap: textureHeight,
+  displacementScale: 0.6,
+  alphaMap: textureAlpha,
+  transparent: true,
+  depthTest: false,
+});
 
 // Mesh
-const sphere = new THREE.Mesh(geometry, material);
-// scene.add(sphere)
+const plane = new THREE.Mesh(geometry, material);
+scene.add(plane);
+
+plane.rotation.x = -1.12;
+gui.add(plane.rotation, 'x').min(-5).max(5).step(0.01).name('Plane rotation X');
 
 // Lights
+const pointLight = new THREE.PointLight(parameters.pointLightColor, 2.28);
 
-const pointLight = new THREE.PointLight(0xffffff, 0.1);
+console.log(parameters.pointLightColor);
+
 pointLight.position.x = 2;
 pointLight.position.y = 3;
-pointLight.position.z = 4;
+pointLight.position.z = 3;
 scene.add(pointLight);
+
+// const ambientLight = new THREE.AmbientLight(0xffffff, 0.3);
+// ambientLight.position.x = 2;
+// ambientLight.position.y = 3;
+// ambientLight.position.z = 4;
+// scene.add(ambientLight);
+
+const groupPointLight = gui.addFolder('Point Light');
+groupPointLight.open();
+
+groupPointLight
+  .add(pointLight, 'intensity')
+  .min(0)
+  .max(5)
+  .step(0.01)
+  .name('Intensity');
+
+groupPointLight
+  .add(pointLight.position, 'x')
+  .min(-10)
+  .max(10)
+  .step(0.01)
+  .name('Position X');
+groupPointLight
+  .add(pointLight.position, 'y')
+  .min(-10)
+  .max(10)
+  .step(0.01)
+  .name('Position Y');
+groupPointLight
+  .add(pointLight.position, 'z')
+  .min(-10)
+  .max(10)
+  .step(0.01)
+  .name('Position Z');
+
+// console.log(parameters.pointLightColor);
+
+groupPointLight.addColor(parameters, 'pointLightColor').onChange(() => {
+  pointLight.color.set(parameters.pointLightColor);
+});
 
 /**
  * Sizes
@@ -66,7 +129,7 @@ const camera = new THREE.PerspectiveCamera(
 );
 camera.position.x = 0;
 camera.position.y = 0;
-camera.position.z = 2;
+camera.position.z = 3;
 scene.add(camera);
 
 // Controls
@@ -78,9 +141,27 @@ scene.add(camera);
  */
 const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
+  alpha: true,
 });
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+/**
+ * Interactivity
+ */
+
+document.addEventListener('mousemove', animateTerrain);
+
+let mouseX = 0;
+let mouseY = 0;
+
+const windowMiddleX = window.innerWidth / 2;
+const windowMiddleY = window.innerHeight / 2;
+
+function animateTerrain(e) {
+  mouseX = e.clientX - windowMiddleX;
+  mouseY = e.clientY;
+}
 
 /**
  * Animate
@@ -92,7 +173,8 @@ const tick = () => {
   const elapsedTime = clock.getElapsedTime();
 
   // Update objects
-  sphere.rotation.y = 0.5 * elapsedTime;
+  plane.rotation.z = 0.25 * elapsedTime;
+  plane.material.displacementScale = 0.3 + mouseY * 0.0008 - mouseX * 0.00025;
 
   // Update Orbital Controls
   // controls.update()
